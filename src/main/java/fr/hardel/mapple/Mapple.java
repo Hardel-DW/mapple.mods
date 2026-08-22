41515dfba45129e75979a6a5752596415656324b
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2026-08-22 21:33:21 +0200

    chore: scaffold the fabric mod template

---
+++ src/main/java/fr/hardel/mapple/Mapple.java
@@ -0,0 +1,15 @@
+package fr.hardel.mapple;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Mapple implements ModInitializer {
+    public static final String MOD_ID = "mapple";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Mapple initialised");
+    }
+}
