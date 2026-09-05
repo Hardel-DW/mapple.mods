fac01f1d685409beb465980bd5faa968eb5747c4
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2026-09-05 22:36:27 +0200

    chore: remove the unused light and persistent map code

--- src/main/java/fr/hardel/mapple/optimisation/light/LightDataListener.java
+++
@@ -1,7 +0,0 @@
-package fr.hardel.mapple.optimisation.light;
-
-import it.unimi.dsi.fastutil.longs.LongSet;
-
-public interface LightDataListener {
-    void mapple$onLightDataChanged(LongSet chunkKeys);
-}
--- src/main/java/fr/hardel/mapple/optimisation/light/SparseDataLayer.java
+++
@@ -1,5 +0,0 @@
-package fr.hardel.mapple.optimisation.light;
-
-public interface SparseDataLayer {
-    void mapple$adopt(byte[] data, int slabs);
-}
